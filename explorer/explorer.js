5a230fc3a53c57b8a4169a03e491f7f12c78085c
iaorana-ora-pf <frise.sante.pf@gmail.com>
2025-06-02 08:22:47 -1000

    Update explorer.js

--- explorer/explorer.js
+++ explorer/explorer.js
@@ -178,7 +178,7 @@ function updateTimeline() {
         e.subject.toLowerCase().includes(filters.search) ||
         e.keywords.some(k => k.toLowerCase().includes(filters.search))
       ))
-    );
+   ));
 
     if (filtered.length) {
       const block = document.createElement("div");
